4e9f845b5a7dac008a130fdecea8249400b3bc63
Paul Ashour <paul.ashour@gmail.com>
2019-04-28 15:17:02 -0600

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,38 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+
+  def self.first_five
+    return self.all.take(5)
+  end
+
+  def self.dinghy
+    return self.all.select{ |b| b.length < 20 }
+  end
+
+  def self.ship
+    return self.all.select{ |b| b.length >= 20 }
+  end    
+
+  def self.last_three_alphabetically
+    return self.all.sort_by{ |b| b.name }.reverse.take(3)
+  end
+
+  def self.without_a_captain
+    return self.all.select{ |b| !b.captain }
+  end
+
+  def self.sailboats
+    return self.all.select{ |b| b.classifications.pluck(:name).include? 'Sailboat' }
+  end
+
+  def self.with_three_classifications
+    return self.all.select{ |b| b.classifications.count == 3 }
+  end
+
+  def classification_names
+    self.classifications.pluck(:name)
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,15 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    return self.select{ |c| c.boats.any?{ |b| b.classifications.any?{ |c| c.name == "Catamaran" }}}
+  end
+
+  def self.sailors
+    return self.select{ |c| c.boats.any?{ |b| b.classifications.any?{ |c| c.name == "Sailboat" }}}
+  end
+
+  def self.talented_seafarers
+    return self.select{ |c| c.boats.classification_names.include?("Motorboat") && c.boats.classification_names.include?("Sailboat") }
+  end
 end
