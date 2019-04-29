d07a6615495903b9dc3f3a2a00b9c5a3633b5eef
Paul Ashour <paul.ashour@gmail.com>
2019-04-28 18:10:09 -0600

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,34 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    take(5)
+  end
+
+  def self.dinghy
+    where("length < ?", 20)
+  end
+
+  def self.ship
+    where("length >= ?", 20)
+  end
+
+  def self.last_three_alphabetically
+    order(:name).reverse_order.take(3)
+  end
+
+  def self.without_a_captain
+    where(captain_id: nil)
+  end
+
+  def self.sailboats
+    includes(:classifications).where('classifications.name = ?', "Sailboat").references(:classifications)
+  end
+
+  def self.with_three_classifications
+    Boat.joins(:classifications).group(:name).having("count(boats.name) >= ?", 3)
+  end
+
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,20 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+
+  def self.catamaran_operators
+    joins(boats: :classifications).group(:name).where("classifications.name = ?", "Catamaran")
+  end
+
+  def self.sailors
+    joins(boats: :classifications).group(:name).where("classifications.name = ?", "Sailboat")
+  end
+
+  def self.talented_seafarers
+    joins(boats: :classifications).where("classifications.name = ?", "Motorboat").group(:name) & self.sailors
+  end
+
+  def self.non_sailors
+    self.all - self.sailors
+  end
 end
